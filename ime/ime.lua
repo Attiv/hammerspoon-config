8b16aac8ca2006f24862a9f8cd35cdf8ca489107
Vitta <wlkhn123@gmail.com>
2020-09-25 10:25:26 +0800

    更新 phpstorm

--- ime/ime.lua
+++ ime/ime.lua
@@ -22,7 +22,7 @@ local app2Ime = {
     {'/Applications/wechatwebdevtools.app', 'English'},
     {'/Applications/Sketch.app', 'English'},
     {'/Applications/Visual Studio Code.app', 'English'},
-    {'/Users/wanglikun/Library/Application Support/JetBrains/Toolbox/apps/PhpStorm/ch-0/202.6948.87/PhpStorm.app', 'English'},
+    {'/Users/wanglikun/Library/Application Support/JetBrains/Toolbox/apps/PhpStorm/ch-0/202.7319.77/PhpStorm.app', 'English'},
     {'/Applications/QQ.app', 'Chinese'},
     {'/Users/wanglikun/Library/Application Support/JetBrains/Toolbox/apps/WebStorm/ch-0/202.7319.49/WebStorm.app', 'English'},
     {'/Applications/Fork.app', 'English'},
