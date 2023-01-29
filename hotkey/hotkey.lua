ea3f807b153d114ac41bf301a11719f6f3fd7012
Vitta <wanglikun@shinetechchina.com>
2023-01-29 22:33:14 +0800

    add call for https://api.shadiao.app/

--- hotkey/hotkey.lua
+++ hotkey/hotkey.lua
@@ -1,7 +1,5 @@
 -- local hyper = {'ctrl', 'cmd'}
-
 -- hs.hotkey.bind({"cmd"}, "L", hs.caffeinate.systemSleep)
-
 -- -- show front activated app infos
 -- hs.hotkey.bind(
 --     hyper, ".",
@@ -12,4 +10,38 @@
 --                                     hs.keycodes.currentSourceID()))
 --     end)
 
+hs.hotkey.bind({"cmd", "shift"}, ",", function()
+    hs.http.asyncGet("https://api.shadiao.pro/chp", nil, function(status, body, headers)
+        if status == 200 then
+            local data = hs.json.decode(body)
+            hs.pasteboard.setContents(data.data.text)
+            hs.alert.show("Copied to clipboard: " .. data.data.text)
+        else
+            hs.alert("Error: " .. status)
+        end
+    end)
+end)
 
+hs.hotkey.bind({"cmd", "shift"}, ".", function()
+    hs.http.asyncGet("https://api.shadiao.pro/pyq", nil, function(status, body, headers)
+        if status == 200 then
+            local data = hs.json.decode(body)
+            hs.pasteboard.setContents(data.data.text)
+            hs.alert.show("Copied to clipboard: " .. data.data.text)
+        else
+            hs.alert("Error: " .. status)
+        end
+    end)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "/", function()
+    hs.http.asyncGet("https://api.shadiao.pro/du", nil, function(status, body, headers)
+        if status == 200 then
+            local data = hs.json.decode(body)
+            hs.pasteboard.setContents(data.data.text)
+            hs.alert.show("Copied to clipboard: " .. data.data.text)
+        else
+            hs.alert("Error: " .. status)
+        end
+    end)
+end)
